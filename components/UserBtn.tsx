24e19c7aa7c050e51cf0d75b5c84006485ea7aa6
rabiul3000 <mdrabiul.asia@gmail.com>
2026-04-17 10:57:09 +0600

    added login logout with supabase auth

--- components/UserBtn.tsx
+++ components/UserBtn.tsx
@@ -1,17 +1,23 @@
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const UserBtn = ({ user }: any) => {
+  console.log(user);
   return (
     <div
-      className="w-5/6 active:w-4/6 text-center
+      className="w-5/6 active:w-4/6 text-center px-2 flex justify-between items-center
            bg-white py-4 rounded-full text-black font-semibold
-           hover:bg-gray-800
+           hover:bg-gray-200
            active:bg-slate-200
            active:text-gray-900
              transition-all
              duration-200"
     >
-      UserBtn
+      <Avatar size="lg">
+        <AvatarImage src={user.user_metadata.avatar_url} alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      
     </div>
   );
 };

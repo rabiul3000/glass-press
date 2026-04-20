9f0fba4babf7d19fd4b1cefa6ea2a021ae56a7b6
rabiul3000 <mdrabiul.asia@gmail.com>
2026-04-20 20:04:58 +0600

    renamed the file for better naming

--- app/api/news/fetch/route.ts
+++
@@ -1,9 +0,0 @@
-import Parser from 'rss-parser';
-
-
-export async function GET() {
-    const parser = new Parser();
-    const feed = await parser.parseURL('https://www.aljazeera.com/news/');
-    return Response.json({ feed })
-}
-
